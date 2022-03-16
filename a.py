932cc0adfa6fbb0560648d142be2c0fa20162779
Lucca Dornelles Cezar <lucca.cezar@edu.pucrs.br>
2022-03-16 11:43:10 -0300

    sada

--- a.py
+++ a.py
@@ -1,8 +1,13 @@
-
-age_in_days = int(input("What's you age in days?"))
-year = int(age_in_days/365)
-rem = age_in_days-(year*365)
-month = int(rem/30)
-rem -= (month*30)
-day = int(rem/1)
-print('Year {} month {} day {}'.format(year,month,day))
+def calcD(age_in_days):
+    year = int(age_in_days/365)
+    rem = age_in_days-(year*365)
+    month = int(rem/30)
+    rem -= (month*30)
+    day = int(rem/1)
+    return (year,month,day)
+        
+age_in_days = int(input(""))
+year, month, day = calcD(age_in_days)
+print(str(year) + ' ano(s)')
+print(str(month) + ' mes(es)')
+print(str(day) + ' dia(s)')
